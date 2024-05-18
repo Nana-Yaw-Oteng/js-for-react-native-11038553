e2a4807fe78e391ba83703a2d27987b1fae24690
Nana-Yaw-Oteng <nyooteng@st.ug.edu.gh>
2024-05-18 15:47:22 +0000

    Task2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,5 +1,17 @@
 function processArray(number){
     const newArray = [];
+for(let arr of number ){
+    if(number % 2 == 0){
+        newArray.push(number * number);
+    }
+    else{
+        newArray.push(pow(number,3));
+    }
+}
+return newArray;
+}
+function processArray(number){
+    const newArray = [];
 for(let number of arr ){
     if(number % 2 == 0){
         newArray.push(pow(number,2));
@@ -9,4 +21,19 @@ for(let number of arr ){
     }
 }
 return newArray;
+}
+function formatArrayStrings(strings,newNum){
+    const newString = [];
+    for(let i=0; i<strings.length;i++){
+        const str = strings[i];
+        const num = newNum[i];
+
+        if(num%2 == 0){
+            newString.push(str.toUpperCase());
+        }
+        else{
+            newString.push(str.toLowerCase());
+        }
+    }
+    return newString;
 }
